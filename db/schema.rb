c4bdc1ce0ac8d7f18bff6461f3a0cb1b7b9c08f8
Amine Boukhriss <boukhrissamine@gmail.com>
2019-08-19 19:06:29 +0200

    addes SSH protection

--- db/schema.rb
+++ db/schema.rb
@@ -93,8 +93,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_162407) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
