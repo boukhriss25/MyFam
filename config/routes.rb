720aa93c7b5dd8fefe6ef0189c8f737dd398e6f9
Amine Boukhriss <boukhrissamine@gmail.com>
2019-08-20 15:37:35 +0200

    routes fam and docs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+
+  resources :families do
+    resources :conversations
+    resources :documents, only: [:new, :create, :index, :show] do
+      collection do
+        get 'search'
+      end
+    end
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
