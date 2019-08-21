f46ce593c14a9ba7440e7d6262fcb3914f303d3e
Glen Warren <glenwarren@zoho.com>
2019-08-21 15:52:44 +0200

    working on dashboard and edit screens

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -6,11 +6,26 @@ class MembershipsController < ApplicationController
   end
 
   def new
+    @membership = Membership.new
   end
 
   def create
+    @family = Family.find(params[:family_id])
+    @membership = Membership.new(membership_params)
+    @membership.family = @family
+    if @membership.save
+      redirect_to family_path(@family)
+    else
+      render 'edit_family_path(@family)'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def membership_params
+    params.require(:membership).permit(:user_id)
   end
 end
