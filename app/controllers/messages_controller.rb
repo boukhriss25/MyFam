e1a897ed5c0cc66c76baa8712c896b4a51193fa7
Wendela Lövgren <wendela.lovgren@gmail.com>
2019-08-21 13:28:24 +0200

    FEAT: messages controller action done except form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,29 @@
 class MessagesController < ApplicationController
+  # before_action :set_message, only:
   def index
+    @messages = Message.where(conversation_id: params[:conversation_id])
   end
 
   def new
+    @conversation = Conversation.find(params[:conversation_id])
+    @message = Message.new
   end
 
   def create
+    @conversation = Conversation.find(params[:conversation_id])
+    @message = Message.new(message_params)
+    @message.user = current_user
+    raise
+    message.save
+  end
+
+  private
+
+  # def set_message
+   # @message = Message.find(params[:id])
+ # end
+
+  def message_params
+    params.require(:message).permit(:content, :conversation_id, :user_id)
   end
 end
