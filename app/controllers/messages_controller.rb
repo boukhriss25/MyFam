6f949d6f316650a2140834499773639c4bf8b7c3
Wendela Lövgren <wendela.lovgren@gmail.com>
2019-08-21 15:33:59 +0200

    FEAT: new message done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,17 @@
 class MessagesController < ApplicationController
   # before_action :set_message, only:
-  def index
-    @messages = Message.where(conversation_id: params[:conversation_id])
-  end
-
-  def new
-    @conversation = Conversation.find(params[:conversation_id])
-    @message = Message.new
-  end
 
   def create
     @conversation = Conversation.find(params[:conversation_id])
+    @family = @conversation.family
     @message = Message.new(message_params)
+    @message.conversation = @conversation
     @message.user = current_user
-    raise
-    message.save
+    if @message.save
+      redirect_to family_conversation_path(@family, @conversation)
+    else
+      render 'conversations/show'
+    end
   end
 
   private
@@ -24,6 +21,6 @@ class MessagesController < ApplicationController
  # end
 
   def message_params
-    params.require(:message).permit(messages[:content], :conversation_id, :user_id)
+    params.require(:message).permit(:content, :conversation_id, :user_id)
   end
 end
