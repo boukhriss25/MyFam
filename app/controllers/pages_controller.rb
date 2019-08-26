f7543540661c73d2fde5d4553842f1da52dda1d4
Mehdi Idrissi <idrissikaitounimehdi@gmail.com>
2019-08-26 18:50:41 +0200

    media home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,17 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def media_home
+    @family = Family.find(params[:family_id])
+    @documents = Document.where(family_id: params[:family_id])
+    @tags = []
+    @documents.each do |doc|
+      doc.tags.each do |tag|
+        @tags.push << tag.name
+      end
+    end
+    @tags = @tags.uniq
+    @document = Document.new
+  end
 end
