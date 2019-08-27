e8c5aac07895ad4ba0f7e422ea67c5f27cdeeae1
Glen Warren <glenwarren@zoho.com>
2019-08-27 21:16:25 +0200

    FIX: set a trim on overflowing sample messages and conversation names on conversations#index

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -7,7 +7,6 @@ class Conversation < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   after_create :default_avatar
 
-  # DEFAULT_AVATAR = "f5psjysh6j1nsoxzqgyx"
   def default_avatar
     if name == "Main chat"
       self.remote_avatar_url = "https://res.cloudinary.com/glenwarren/#{family[:avatar]}"
