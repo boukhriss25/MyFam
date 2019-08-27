15b7962cfed2a998db9c038c563dd03c0fcf6025
Amine Boukhriss <boukhrissamine@gmail.com>
2019-08-27 12:30:56 +0200

    FEAT: added websocket production setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+config.action_cable.url = "wss://www.myfam.live/cable"
+config.action_cable.allowed_request_origins = [ "http://www.myfam.live", "https://www.myfam.live" ]
+
 end
