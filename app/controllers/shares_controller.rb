c295c4bc3636fb42dcf3d4aae00d2f98b2795840
Glen Warren <glenwarren@zoho.com>
2019-08-28 14:15:01 +0200

    FEAT: added a shares#index view

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,5 +1,7 @@
 class SharesController < ApplicationController
   def index
+    @family = Family.find(params[:family_id])
+    @shares = Share.where(conversation: params[:conversation_id])
   end
 
   def new
