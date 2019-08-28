00b689c8e0c35cb0895c5412d04d778f39788d63
Glen Warren <glenwarren@zoho.com>
2019-08-28 14:26:11 +0200

    FIX: few small changes, fixed the back button

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,6 +1,7 @@
 class SharesController < ApplicationController
   def index
     @family = Family.find(params[:family_id])
+    @conversation = Conversation.find(params[:conversation_id])
     @shares = Share.where(conversation: params[:conversation_id])
   end
 
