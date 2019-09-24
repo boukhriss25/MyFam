484e40f3aed9cb7fce01529c365a3c026ab52a16
Glen Warren <glenwarren@zoho.com>
2019-09-24 23:23:36 +0100

    FIX: working on notes CRUD

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,18 +2,28 @@ class NotesController < ApplicationController
   before_action :set_family, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   def index
-    @collaborations = Collaboration.where(user: current_user).select do |subsc|
-      Collaboration.find(subsc.collaboration_id).family_id == @family.id
+    @collaborations = Collaboration.where(user: current_user).select do |collab|
+      Note.find(collab.note_id).family_id == @family.id
     end
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    @note.family = @family
+    @collaboration = Collaboration.new(user: current_user, note: @note)
+    if @note.save && @collaboration.save
+      redirect_to family_note_path(@family, @note)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,11 +33,17 @@ class NotesController < ApplicationController
   end
 
   def destroy
+    @note.destroy
+    redirect_to family_notes_path(@family)
   end
 
   private
 
   def set_family
     @family = Family.find(params[:family_id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title)
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,11 @@
 class Note < ApplicationRecord
-  belongs_to :user
   belongs_to :family
   has_many :collaborations, dependent: :destroy
   has_many :users, through: :collaborations
   validates :title, presence: true
+  # after_create :default_title
+
+  # def default_title
+  #   Tag.create(name: "untitled", document: self)
+  # end
 end
