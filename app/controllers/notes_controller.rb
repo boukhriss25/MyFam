59aaf9d99919a46461cf7df36e34de828d957416
Glen Warren <glenwarren@zoho.com>
2019-09-24 19:19:20 +0100

    FEAT: started building notes#index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,10 @@
 class NotesController < ApplicationController
+  before_action :set_family, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   def index
+    @collaborations = Collaboration.where(user: current_user).select do |subsc|
+      Collaboration.find(subsc.collaboration_id).family_id == @family.id
+    end
   end
 
   def show
@@ -18,5 +23,11 @@ class NotesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_family
+    @family = Family.find(params[:family_id])
   end
 end
